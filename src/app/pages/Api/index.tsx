a15db84eec09aefe3115e7ac873b0414a8f55920
Luciel Vitor BS <luciel.vitor.bs@gmail.com>
2022-01-03 11:03:58 -0300

    remove warning

--- src/app/pages/Api/index.tsx
+++ src/app/pages/Api/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { apiConstants } from "../../../api/constants";
 import { ipcRenderer } from "../../../preload/constants";
