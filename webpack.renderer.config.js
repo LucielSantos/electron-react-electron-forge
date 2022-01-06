342ab74c081f70b4478c107d25e208fcf6fbd1a3
Luciel Vitor BS <luciel.vitor.bs@gmail.com>
2022-01-05 23:20:43 -0300

    configure relative path

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,9 +1,10 @@
-const rules = require('./webpack.rules');
-const plugins = require('./webpack.plugins');
+const path = require("path");
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 module.exports = {
@@ -12,6 +13,9 @@ module.exports = {
   },
   plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+    alias: {
+      src: path.resolve(__dirname, "src"),
+    },
   },
 };
